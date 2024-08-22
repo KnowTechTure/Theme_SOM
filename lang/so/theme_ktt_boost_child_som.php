b43e40738e68c324d9e8aadf274578fe26872bab
Maybelline Portillo <portillom634@gmail.com>
2024-08-22 11:36:32 -0600

    Upload the latest version.

---
+++ lang/so/theme_ktt_boost_child_som.php
@@ -0,0 +1,71 @@
+<?php
+// Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
+
+// This line protects the file from being accessed by a URL directly.
+defined('MOODLE_INTERNAL') || die();
+
+// A description shown in the admin theme selector.
+$string['choosereadme'] = 'Mawduuca ktt_boost_child_som waa mawduuca ilmaha ee Kobcinta. Waxay ku darsataa awoodda lagu dhejiyo sawirada asalka ah.';
+// The name of our plugin.
+$string['pluginname'] = 'ktt_boost_child_som theme';
+// We need to include a lang string for each block region.
+$string['region-side-pre'] = 'Sax';
+// The name of the second tab in the theme settings.
+$string['advancedsettings'] = 'Dejinta horumarsan';
+// The brand colour setting.
+$string['brandcolor'] = 'Midabka astaanta';
+// The brand colour setting description.
+$string['brandcolor_desc'] = 'Midabka lahjadda.';
+// Name of the settings pages.
+$string['configtitle'] = 'ktt_boost_child_som goobaha';
+// Name of the first settings tab.
+$string['generalsettings'] = 'Goobaha guud';
+// Preset files setting.
+$string['presetfiles'] = 'Mawduuc dheeri ah faylal horudhac ah';
+// Preset files help text.
+$string['presetfiles_desc'] = 'Faylasha hore loo dejiyay waxa loo isticmaali karaa in si wayn loo beddelo muuqaalka mawduuca. Eeg <a href=https://docs.moodle.org/dev/Boost_Presets>Boost presets</a> macluumaadka ku saabsan abuurista iyo wadaaga faylalkaaga hore, oo arag <a href=http://moodle.net/boost>Presets repository</a> Presets in kuwa kale la wadaago.';
+// Preset setting.
+$string['preset'] = 'Mawduuca horudhaca';
+// Preset help text.
+$string['preset_desc'] = 'Dooro horudhac si aad si ballaadhan u beddesho muuqaalka mawduuca.';
+// Raw SCSS setting.
+$string['rawscss'] = 'CSS ceeriin';
+// Raw SCSS setting help text.
+$string['rawscss_desc'] = 'Isticmaal goobtan si aad u bixiso SCSS ama code CSS kaas oo lagu duri doono dhamaadka xaashida qaabka.';
+// Raw initial SCSS setting.
+$string['rawscsspre'] = 'SCSS bilawga ceeriin';
+// Raw initial SCSS setting help text.
+$string['rawscsspre_desc'] = 'Goobtan waxaad ku siin kartaa bilowga code SCSS, waxa lagu duri doonaa wax kasta oo kale ka hor. Inta badan waxaad isticmaali doontaa goobtan si aad u qeexdo doorsoomayaasha.';
+
+// home
+$string['home_slid1_title'] = 'Aqoon Tayo leh';
+$string['home_slid1_subTitle'] = 'si loo helo daryeel caafimaad oo wanaagsan';
+
+$string['home_slid2_title'] = 'Hab waara oo isku xidhan';
+$string['home_slid2_subTitle'] = 'si loo dhiso awoodda fog';
+
+$string['home_slid3_title'] = 'Waara oo la isku duwo';
+$string['home_slid3_subTitle'] = 'hab dhisidda awoodda fog';
+
+$string['home_slid4_title'] = 'Lorem ipsum dolor sit amet';
+$string['home_slid4_subTitle'] = 'Nullam bibendum est ex, nec commodo nibh';
+
+$string['home_slid5_title'] = 'Quality knowledge for better health care';
+$string['home_slid5_subTitle'] = 'Nullam bibendum est ex, nec commodo nibh';
+
+// Footer
+
+$string['footer_titleMenu1'] = 'Bogagga';
+$string['footer_menu1Item1'] = 'Guriga';
+$string['footer_menu1Item2'] = 'La xidhiidh';
+$string['footer_menu1Item3'] = 'Ku saabsan';
+
+$string['footer_titleMenu2'] = 'XIRIIR FIICAN';
+$string['footer_menu2Item1'] = 'Qaanuunka Arrimaha Khaaska ah';
+$string['footer_menu2Item2'] = 'Shuruudaha';
+
+$string['footer_orgData'] = 'POPULATION SERVICES INTERNATIONAL SOMALIA | ';
+$string['footer_contactNumberLb'] = 'Wac:';
+
+$string['footer_links'] = '';
+$string['footer_CPRS'] = '2024 - Soomaaliya.';
